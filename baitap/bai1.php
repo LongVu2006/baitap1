37659938cc7eb840f6166f5d49b39880cce09c72
LongVu2006 <tranvu21193@gmail.com>
2021-11-13 11:57:38 +0700

    update

---
+++ baitap/bai1.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="icon" href="../image/icon-thanh-url.svg">
+	<link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../lib/css/menu.css">
+	<link rel="stylesheet" href="../lib/css/about-app.css">
+	<link rel="stylesheet"  href="../lib/css/center.css">
+	<link rel="stylesheet"  href="../lib/css/set-up.css">
+	<link rel="stylesheet"  href="../lib/css/application-notification.css">
+	<link rel="stylesheet"  href="../lib/css/download-an-app.css">
+	<link rel="stylesheet"  href="../lib/css/relationship.css">
+	<link rel="stylesheet"  href="../lib/css/footer.css">
+</head>
+<body>
+	<div class="container">
+		<!-- Header -->
+		<div class="row">
+
+							<!-- Menu Flex -->
+			<div class="col menu">
+						<!-- Logo and Menu -->
+				<div class="row">
+					<img id="logo" src="../image/logo.svg">
+					<div class="flex-menu">
+					 	<div><span id="home">Home</span></div>
+					 	<div><span>Features</span></div>
+					 	<div><span>About Us</span></div>
+					 	<div><img id="vector" src="../image/vector.svg"></div>
+					 </div>
+				</div>
+							<!-- Text -->
+				<div class="row">
+						<p class="text-body" id="manage">Manage Schedule and Task Easy</p>
+						<p class="text-muted" id="create">Create and organize schedule and task deadline become better, easy manage and certainly no longer lagging behind schedule of activities and task</p>
+						<a href="#" id="download-app">Download App</a>
+				</div>
+			</div>
+
+					<!-- About App -->
+			<div class="col about-app">
+				<a id="about-app" href="#">About App</a>
+						<!-- Mobile -->
+				<div class="mobile">
+					<img id="mobile" src="../image/mobile.png">
+				</div>
+				<div id="easy-to-use">
+					<p id="easy-to-use-1">Easy</p>
+					<p id="easy-to-use-2">to use</p>
+				</div>
+				<div id="an-interesting">
+					<p id="an-interesting-1">An interesting</p>
+					<p id="an-interesting-2">UI</p>
+				</div>
+				<div id="user-download">
+					<img src="../image/user.svg" id="user">
+					<p id="user-download-1">User Download</p>
+					<p id="user-download-2">50.000</p>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- Schedule -->
+		<div class="row schedule">
+					<!-- Deadline -->
+			<div class="col deadline">
+				<img src="../image/deadline.png" id="deadline">
+				<p class="text-body" id="deadline-1">Set a better deadline tasks</p>
+				<p class="text-muted" id="deadline-2">You can make your duty deadlines better and more orderly</p>
+			</div>
+					<!-- Manage -->
+			<div class="col manage">
+				<img src="../image/manage.png" id="Manage">
+				<p class="text-body" id="manage-1">Manage the tasks so easily</p>
+				<p class="text-muted" id="manage-2">You can manage the deadlines and the schedule becomes easy</p>
+			</div>
+					<!-- Notification -->
+			<div class="col notification">
+				<img src="../image/notification.png" id="notification">
+				<p class="text-body" id="notification-1">Wouldn’t miss notifications</p>
+				<p class="text-muted" id="notification-2">Of course you will always get the tasks and schedule notifications</p>
+			</div>
+		</div>
+
+		<!-- Set Up -->
+		<div class="row set-up">
+						<!-- Picture -->
+			<div class="col set-up-1">
+				<img src="../image/create1.png" id="create1">
+				<img src="../image/create2.png" id="create2">
+				<img src="../image/create3.png" id="create3">
+			</div>
+						<!-- Text -->
+			<div class="col set-up-2">
+				<p class="text-body set-up-task">Set up task and schedule with a best features</p>
+				<p class="text-muted the-best-feature">The best feature makes you better, with the latest technology to meet your needs. Of course, with such features as organizing schedules and leading assignments, it will make you easier, So get the application now !</p>
+			</div>
+		</div>
+
+		<!-- Application Notification -->
+		<div class="row application-notification">
+						<!-- Text -->
+			<div class="col application-notification-1">
+				<p class="text-body get-the-highest">Get the highest priority application notification</p>
+				<p class="text-muted application-now">The best feature makes you better, with the latest technology to meet your needs. Of course, with such features as organizing schedules and leading assignments, it will make you easier, So get the application now !</p>
+			</div>
+
+						<!-- Get The Highest -->
+			<div class="col application-notification-2">
+				<img src="../image/get1.png" id="get1">
+				<img src="../image/get2.png" id="get2">
+			</div>
+			
+		</div>
+
+		<!-- Download An App -->
+		<div class="row download-an-app">
+			<div class="row download-an-app-1">
+				<div id="download">Download an app now and the various benefits you will get immediately</div>
+			</div>
+			<div class="row download-an-app-2">
+					<a id="down" href="#">Download App</a>
+					<a id="about" href="#">About App</a>
+			</div>
+		</div>
+
+		<!-- Relationship -->
+		<div class="row relationship">
+						<!-- Picture -->
+			<div class="col relationship-1">
+				<img src="../image/relationship.png" id="relationship">
+			</div>
+						<!-- Text -->
+			<div class="col relationship-2">
+					<img src="../image/person.png" id="person">
+					<p id="wh-quetion">What do they think</p>
+					<p id="samantha-sench">Samantha Sench</p>
+					<p id="student">Student at University</p>
+					<p id="greeting">“ Hi, it’s Samantha. After using it made me a lot of benefits, starting with convenience of setting a deadline of tasks and schedule after that the app is very simple using. “</p>
+					<a href="#"><img src="../image/go1.svg" id="go1"></a>
+					<a href="#"><img src="../image/go2.svg" id="go2"></a>
+			</div>
+		</div>
+
+		<!-- Footer -->
+		<div class="row footer">
+					<!-- Menu -->
+			<div class="row menu-footer">
+				<div class="col-xl-3">
+					<img src="../image/logo.svg" id="logo-footer">
+				</div>
+				<div class="col-xl-3">
+					Sitemaps
+				</div>
+				<div class="col-xl-3">
+					Advantages
+				</div>
+				<div class="col-xl-3">
+					Follow Us
+				</div>
+			</div>
+					<!-- Li ul -->
+			<div class="row li-ul">
+				<div class="col-xl-3">
+					<span>Task deadlines and scheduling applications with various benefits and advantages</span>
+				</div>
+				<div class="col-xl-3">
+					<ul id="foot4">
+						<li><span>Home</span></li>
+						<li><span>Features</span></li>
+						<li><span>About Us</span></li>
+					</ul>
+				</div>
+				<div class="col-xl-3">
+					<ul id="foot6">
+						<li><span>Set Task Better</span></li>
+						<li><span>Easy Manage</span></li>
+						<li><span>Get Notification</span></li>
+					</ul>
+				</div>
+				<div class="col-xl-3">
+					<img src="../image/face.svg" class="face">
+					<img src="../image/in.svg" class="in">
+					<img src="../image/twitch.svg" class="twitch">
+				</div>
+			</div>
+		</div>
+	</div>
+
+<script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+<script src=""></script>
+<script src=""></script>
+</body>
+
+</html>
